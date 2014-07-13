83e254b9bf2e561ea7f6fb809d5daa09e8bfd63c
miftahirfan <miftahirfan@gmail.com>
2014-07-13 15:56:18 +0700

    test

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link href="asset/dist/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="asset/dist/css/bootstrap-theme.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+        	<h1> Tittle </h1>
+        </div>
+        
+        <div class="slide">
+        	<h2> Slide </h2>
+        </div>
+        
+        <div class="content">
+        	<h3>Content</h3>
+        </div>
+        
+        <div class="footer">
+        	<h4>Footer</h4>
+        </div>
+    </div>
+</body>
+</html>
